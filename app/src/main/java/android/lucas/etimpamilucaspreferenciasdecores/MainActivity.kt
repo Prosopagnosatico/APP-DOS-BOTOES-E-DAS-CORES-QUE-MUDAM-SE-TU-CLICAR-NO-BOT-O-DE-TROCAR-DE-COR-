ea2a96505efda99570d84118af17a71ea9821a37
Lucas Camilo <Lucas.sousa3232etec.sp.gpv.br>
2025-06-27 22:50:57 -0300

    Commitada final

--- app/src/main/java/android/lucas/etimpamilucaspreferenciasdecores/MainActivity.kt
+++ app/src/main/java/android/lucas/etimpamilucaspreferenciasdecores/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import android.graphics.Color
 import android.lucas.etimpamilucaspreferenciasdecores.databinding.ActivityMainBinding
 import android.view.inputmethod.InputBinding
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        const val NOME_ARQUIVO = "arquivo_prefs.xml"
+    }
 
     private var cor = ""
 
@@ -25,29 +33,74 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
-        binding.cor1.setOnClickListener{
+        binding.cor1.setOnClickListener {
             cor = "#FF0000"
+            salvarCor(cor)
         }
 
-        binding.cor2.setOnClickListener{
+        binding.cor2.setOnClickListener {
             cor = "#00DDFF"
+            salvarCor(cor)
         }
 
-        binding.cor3.setOnClickListener{
+        binding.cor3.setOnClickListener {
             cor = "#08E700"
+            salvarCor(cor)
         }
 
-        binding.cor4.setOnClickListener{
+        binding.cor4.setOnClickListener {
             cor = "FFE400"
-        }
-        binding.cor5.setOnClickListener{
-            cor = "#673AB7"
+            salvarCor(cor)
         }
 
-        binding.cor6.setOnClickListener{
-            cor = "FF5722"
+        binding.cor5.setOnClickListener {
+            cor = "#673AB7"
+            salvarCor(cor)
         }
+
+        binding.cor6.setOnClickListener {
+            cor = "FF5722"
+            salvarCor(cor)
+        }
+
 
 
     }
-}
+
+    private fun salvarCor(cor: String) {
+
+        binding.main.setBackgroundColor(Color.parseColor(cor))
+
+        binding.btnTrocar.setOnClickListener { view ->
+            val preferencias = getSharedPreferences(NOME_ARQUIVO, MODE_PRIVATE)
+            val editor = preferencias.edit()
+            editor.putString("cor", cor)
+            editor.putString("nome", "Lucas")
+            editor.putString("sobrenome", "Camilo")
+            editor.putInt("Idade", 16)
+            editor.apply()
+
+            snackBar(view)
+        }
+    }
+
+            private fun snackBar(view: View) {
+                val snackbar = Snackbar.make(
+                    view,
+                    "Cor de fundo alterada com sucesso!",
+                    Snackbar.LENGTH_INDEFINITE
+                )
+                snackbar.setAction("OK") {
+
+                }
+
+                snackbar.setActionTextColor(Color.BLUE)
+                snackbar.setBackgroundTint(Color.LTGRAY)
+                snackbar.setTextColor(Color.GREEN)
+                snackbar.show()
+
+            }
+
+
+        }
+
